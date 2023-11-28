82120247332def1e30c82e049ce1f538a7074d8b
Gustavo Juan <gustavojuandev@gmail.com>
2023-11-28 10:57:54 +0100

    [LESSON] Different Classes to model concepts

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
 <?php
+class InvoiceItem
+{
+
+}
+class Invoice
+{
+
+}
+class Route
+{
+
+}
+class User
+{
+
+}
+ class AchievementBadge
+ {
+    public $title;
+    public $description;
+    public $points;
+
+
+    public function awardTo($user)
+    {
+
+    }
+
+ }
+
