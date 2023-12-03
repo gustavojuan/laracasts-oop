a7ea5ac9c5745d0285af3cacf70ac8233a64b7a9
gustavojuan <gustavojuan@gmail.com>
2023-12-03 10:00:48 +0100

    [Abstract] Badges Abstract Class

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php
 
-
-class AchievementType
+abstract class AchievementType
 {
     public function name()
     {
         $class = (new ReflectionClass($this))->getShortName();
-        return trim(preg_replace('/[A-Z]/', ' $0',$class));
+        return trim(preg_replace('/[A-Z]/', ' $0', $class));
     }
 
     public function icon()
     {
-        return  strtolower(str_replace(' ','-',$this->name())).'.png';
+        return strtolower(str_replace(' ', '-', $this->name())) . '.png';
     }
+
+    abstract public function qualifier($user);
 
 }
 
@@ -20,7 +21,7 @@ class FirstThousandPoints extends AchievementType
 {
     public function qualifier($user)
     {
-
+        throw new \Exception('qualifer not imnplemented');
     }
 }
 
@@ -32,10 +33,19 @@ class FirstBestAnswer extends AchievementType
     }
 }
 
-$ftp  = new FirstThousandPoints();
+
+class ReachTop50 extends AchievementType
+{
+    public function qualifier($user)
+    {
+
+    }
+}
+
+$ftp = new FirstThousandPoints();
 echo $ftp->name();
 echo $ftp->icon();
 
 $fba = new FirstBestAnswer();
 echo $fba->name();
-echo  $fba->icon();
+echo $fba->icon();
