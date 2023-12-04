72d5c26f7ff0bfd405309d23cf5f4229ac5d213c
Gustavo Juan <gustavojuandev@gmail.com>
2023-12-04 10:14:45 +0100

    [LESSON] Encapsulation 1

--- index.php
+++ index.php
@@ -1,41 +1,42 @@
 <?php
 
-interface Newsletter
-{
-    public function subscribe($email);
-}
+// Encapsulation => Enclose within a capsule
 
-class CampaignMonitor implements Newsletter
+class Person
 {
-    public function subscribe($email)
+
+
+    public $name;
+
+    public function __construct($name)
     {
-        die('Subs with CampaignMonitor');
+        $this->name = $name;
+    }
+
+    public function job()
+    {
+        return 'software developer';
+    }
+
+    public function favoriteTeam()
+    {
+
+    }
+
+    private function thingsThatKeepUpAtNight()
+    {
+        return 'cosas que a lo mejor no todo el mundo debiera saber';
     }
 }
 
+//$method = new \ReflectionMethod(Person::class,'thingsThatKeepUpAtNight');
+//var_dump($method);
+//$method->setAccessible(1);
+//die();
+//
+//$bob = new Person('Bob');
+//var_dump($bob->thingsThatKeepUpAtNight());
 
-class Drip implements Newsletter
-{
-    public function subscribe($email)
-    {
-        die('Subns with Drip');
-    }
-}
-
-
-
-
-
-class NewsletterSubscriptionController
-{
-    public function store( Newsletter  $newsletter)
-    {
-
-        $newsletter->subscribe('testemail');
-    }
-}
-
-$controller = new NewsletterSubscriptionController();
-
-//$controller->store(new Drip());
-$controller->store(new CampaignMonitor());
+$person = new Person('Bob');
+$person->name = null;
+var_dump($person);
