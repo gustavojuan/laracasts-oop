ffe5010d4b64328e453ef3143c0b85eceac499a1
Gustavo Juan <gustavojuandev@gmail.com>
2023-12-05 08:21:29 +0100

    [LESSON] Object Compositions and Abstractions 1

--- index.php
+++ index.php
@@ -1,34 +1,81 @@
 <?php
-class Tennis
+class Subscription
 {
+    protected StripeGetaway $stripeGetaway;
 
-    protected $playerOne;
-
-    /** Affordance method */
-    public function score()
+    public function __construct(StripeGetaway $stripeGetaway)
     {
-        //is there a winner
-
-        //does someone have an advantage
-
-        //are they in deuce
+        $this->stripeGetaway = $stripeGetaway;
     }
 
-
-
-    protected function hasWinner()
+    public function create()
     {
 
     }
 
-    protected function hasAdvantage()
+    public function cancel()
+    {
+        //api request
+            // find stripe customer
+            //$customer = $this->stripeGetaway->findStripeCustomer();
+
+            // find stipe subscription by customer
+            //$this->stripeGetaway->findStripeSubscriptionByCustomer($customer);
+    }
+
+    public function invoice()
     {
 
     }
 
-    protected function inDeuce()
+    public function  swap($newPlan)
     {
 
     }
 
+
+    /*INFO: Estos dos métodos en esta clase chirrían*/
+//    protected function findStripeCustomer()
+//    {
+//
+//    }
+//
+//
+//    protected function findStripeSubscriptionByCustomer()
+//    {
+//
+//    }
+
+}
+
+
+//class BillableSubscription extends Subscription
+//{
+//
+//    /*INFO: Estos dos métodos estarían más relacionados con otra clase*/
+//    protected function findStripeCustomer()
+//    {
+//
+//    }
+//
+//
+//    protected function findStripeSubscriptionByCustomer()
+//    {
+//
+//    }
+//}
+
+
+class StripeGetaway
+{
+    public function findStripeCustomer()
+    {
+
+    }
+
+
+    public  function findStripeSubscriptionByCustomer()
+    {
+
+    }
 }
