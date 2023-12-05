a263591ecc85108edd21d8d655a1b69ab76b5c3f
Gustavo Juan <gustavojuandev@gmail.com>
2023-12-05 11:09:34 +0100

    [LESSON] Exceptions 3

--- index.php
+++ index.php
@@ -1,40 +1,66 @@
 <?php
 
-class Video
+
+class MaximumMembersReached extends Exception
 {
+    protected $message = 'You may not add more than 3 team members';
+}
+
+class Member
+{
+    public $name;
+
+    /**
+     * @param $name
+     */
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
 
 }
 
-class User
+class Team
 {
-    public function download(Video $video)
+
+    protected $members = [];
+
+    public function add(Member $member)
     {
-        if(!$this->subscribed()) {
-            throw new Exception('You must be susbscribed to download videos');
+
+        if (count($this->members) === 3){
+            throw new MaximumMembersReached();
         }
+        $this->members[] = $member;
     }
 
-    public function subscribed ()
+    public function members()
     {
-            return false;
+        return $this->members;
     }
 }
 
-
-class UserDownloadsController
+class TeamMembersController
 {
-    public function show()
+    public function store()
     {
+        $team = new Team; //has a maximum of 3 members
 
-        try
-        {
-            (new User)->download(new Video);
-        }catch ( Exception $e) {
-            echo "Well". $e->getMessage();
+        try {
+            $team->add(new Member('john doe'));
+            $team->add(new Member('jane doe'));
+            $team->add(new Member('frank doe'));
+            $team->add(new Member('susan doe'));
+            var_dump($team->members());
+
+        }catch (MaximumMembersReached $e)    {
+            var_dump($e->getMessage());
         }
 
+
     }
 }
 
-
+(new TeamMembersController)->store();
 
